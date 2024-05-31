2b0b87bbc670c415376dbf245276e8c60483adfd
bugstars <ren.pan@qq.com>
2024-05-31 18:01:02 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
+import {StatusBar} from "expo-status-bar";
+import {StyleSheet, View} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from "./components/EmojiList";
+import EmojiSticker from './components/EmojiSticker';
+
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [showAppOptions, setShowAppOptions] = useState(false);
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const [pickedEmoji, setPickedEmoji] = useState(null);
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+            setShowAppOptions(true);
+        } else {
+            alert('你没有选择任何图片');
+        }
+    };
+
+    const onReset = () => {
+        setShowAppOptions(false);
+    };
+
+    const onAddSticker = () => {
+        setIsModalVisible(true);
+    };
+
+    const onSaveImageAsync = async () => {
+        alert('你选择了保存图片');
+    };
+
+    const onModalClose = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <GestureHandlerRootView style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+                {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
+            </View>
+            {showAppOptions ? (
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton icon="refresh" label="重置" onPress={onReset}/>
+                        <CircleButton onPress={onAddSticker}/>
+                        <IconButton icon="save-alt" label="保存" onPress={onSaveImageAsync}/>
+                    </View>
+                </View>
+            ) : (
+                <View style={styles.footerContainer}>
+                    <Button theme="primary" onPress={pickImageAsync} label="选择一张照片"/>
+                    <Button label="选用这张照片" onPress={() => setShowAppOptions(true)}/>
+                </View>
+            )}
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+            </EmojiPicker>
+            <StatusBar style="auto"/>
+        </GestureHandlerRootView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
+    optionsContainer: {
+        position: 'absolute',
+        bottom: 80,
+    },
+    optionsRow: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
 });
